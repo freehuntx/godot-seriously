748ce79269a39db5928150f65576d1db6a910a53
freehuntx <freehuntx@web.de>
2026-03-13 03:55:47 +0100

    break: Improve performance, reduce size, add vector4

--- addons/Seriously/Seriously.gd
+++ addons/Seriously/Seriously.gd
@@ -1,11 +1,6 @@
 class_name Seriously extends RefCounted
 
-const OBJECT_AS_DICT := false # Should objects be serialized as dict? (more performance?)
-const INT_MAX = 9223372036854775807
-const INT_MIN = -9223372036854775808
-
 enum CustomTypes {
-	# Ensure the values dont overlap with an existing TYPE_*
 	UINT8 = 50,
 	INT8 = 51,
 	UINT16 = 52,
@@ -25,7 +20,7 @@ static var _serializer = {
 			return null,
 	},
 	TYPE_BOOL: {
-		"pack": func(value, stream: StreamPeerBuffer) -> StreamPeerBuffer:
+		"pack": func(value: bool, stream: StreamPeerBuffer) -> StreamPeerBuffer:
 			stream.put_u8(1 if value else 0)
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> bool:
@@ -103,11 +98,12 @@ static var _serializer = {
 	},
 	TYPE_STRING: {
 		"pack": func(value: String, stream: StreamPeerBuffer) -> StreamPeerBuffer:
-			stream.put_u16(value.length())
-			stream.put_data(value.to_utf8_buffer())
+			var bytes = value.to_utf8_buffer()
+			_put_dynamic_int(bytes.size(), stream)
+			stream.put_data(bytes)
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> String:
-			return stream.get_utf8_string(stream.get_u16()),
+			return stream.get_utf8_string(_get_dynamic_int(stream)),
 	},
 	TYPE_VECTOR2: {
 		"pack": func(value: Vector2, stream: StreamPeerBuffer) -> StreamPeerBuffer:
@@ -267,9 +263,9 @@ static var _serializer = {
 			return NodePath(unpack(stream, TYPE_STRING)),
 	},
 	TYPE_RID: {
-		"pack": func(_value: RID, _stream: StreamPeerBuffer) -> StreamPeerBuffer:
+		"pack": func(_value: RID, stream: StreamPeerBuffer) -> StreamPeerBuffer:
 			push_error("[Seriously] TYPE_RID is not supported")
-			return null,
+			return stream,
 		"unpack": func(_stream: StreamPeerBuffer):
 			push_error("[Seriously] TYPE_RID is not supported")
 			return null,
@@ -277,242 +273,191 @@ static var _serializer = {
 	TYPE_OBJECT: {
 		"pack": func(value: Object, stream: StreamPeerBuffer) -> StreamPeerBuffer:
 			var prop_names = value.get_property_list().filter(func(p): return p.usage & PROPERTY_USAGE_SCRIPT_VARIABLE).map(func(p): return p.name)
-
-			stream.put_u16(prop_names.size())
-
+			_put_dynamic_int(prop_names.size(), stream)
+			
 			for name in prop_names:
 				pack(name, stream, TYPE_STRING)
 				pack(value.get(name), stream)
-
 			return stream,
-		"unpack": func(stream: StreamPeerBuffer):
-			var object_size := stream.get_u16()
+		"unpack": func(stream: StreamPeerBuffer) -> Dictionary:
+			var object_size = _get_dynamic_int(stream)
 			var dict := {}
-
+			
 			for j in object_size:
 				var name = unpack(stream, TYPE_STRING)
 				dict[name] = unpack(stream)
-
-			if OBJECT_AS_DICT:
-				return dict
-
-			# Create dynamic object (bad performance?)
-			var source_code := "extends RefCounted\n"
-
-			for name in dict.keys():
-				source_code += "var %s\n" % [name]
-
-			var dynamic_object := GDScript.new()
-			dynamic_object.source_code = source_code
-			dynamic_object.reload()
-
-			var object = dynamic_object.new()
-			for name in dict.keys():
-				object.set(name, dict[name])
-
-			return object,
+				
+			return dict,
 	},
 	TYPE_CALLABLE: {
-		"pack": func(_value: Callable, _stream: StreamPeerBuffer) -> StreamPeerBuffer:
+		"pack": func(_value: Callable, stream: StreamPeerBuffer) -> StreamPeerBuffer:
 			push_error("[Seriously] TYPE_CALLABLE type pack requested. This is not possible!")
-			return null,
+			return stream,
 		"unpack": func(_stream: StreamPeerBuffer):
 			push_error("[Seriously] TYPE_CALLABLE type unpack requested. This is not possible!")
 			return null,
 	},
 	TYPE_SIGNAL: {
-		"pack": func(_value: Signal, _stream: StreamPeerBuffer) -> StreamPeerBuffer:
+		"pack": func(_value: Signal, stream: StreamPeerBuffer) -> StreamPeerBuffer:
 			push_error("[Seriously] TYPE_SIGNAL type pack requested. This is not possible!")
-			return null,
+			return stream,
 		"unpack": func(_stream: StreamPeerBuffer):
 			push_error("[Seriously] TYPE_SIGNAL type unpack requested. This is not possible!")
 			return null,
 	},
 	TYPE_DICTIONARY: {
 		"pack": func(value: Dictionary, stream: StreamPeerBuffer) -> StreamPeerBuffer:
-			stream.put_u16(value.size())
-
+			_put_dynamic_int(value.size(), stream)
 			for key in value.keys():
-				pack(key, stream, TYPE_STRING)
+				pack(key, stream)
 				pack(value[key], stream)
-
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> Dictionary:
-			var dictionary_size = stream.get_u16()
+			var dictionary_size = _get_dynamic_int(stream)
 			var dictionary = {}
-
 			for j in dictionary_size:
-				var name = unpack(stream, TYPE_STRING)
-				dictionary[name] = unpack(stream)
-
+				var key = unpack(stream)
+				dictionary[key] = unpack(stream)
 			return dictionary,
 	},
 	TYPE_ARRAY: {
 		"pack": func(value: Array, stream: StreamPeerBuffer) -> StreamPeerBuffer:
-			var array_size := value.size()
-
-			stream.put_u16(array_size)
-
-			for i in array_size:
-				pack(value[i], stream)
-
+			_put_dynamic_int(value.size(), stream)
+			for item in value:
+				pack(item, stream)
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> Array:
-			var array_size = stream.get_u16()
+			var array_size = _get_dynamic_int(stream)
 			var array = []
-
 			for i in array_size:
 				array.append(unpack(stream))
-
 			return array,
 	},
 	TYPE_PACKED_BYTE_ARRAY: {
 		"pack": func(value: PackedByteArray, stream: StreamPeerBuffer) -> StreamPeerBuffer:
-			stream.put_u16(value.size())
+			_put_dynamic_int(value.size(), stream)
 			stream.put_data(value)
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> PackedByteArray:
-			var array_size := stream.get_u16()
+			var array_size = _get_dynamic_int(stream)
 			var data = stream.get_data(array_size)[1]
 			return PackedByteArray(data),
 	},
 	TYPE_PACKED_INT32_ARRAY: {
 		"pack": func(value: PackedInt32Array, stream: StreamPeerBuffer) -> StreamPeerBuffer:
 			var buffer = value.to_byte_array()
-
-			stream.put_u16(buffer.size())
+			_put_dynamic_int(buffer.size(), stream)
 			stream.put_data(buffer)
-
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> PackedInt32Array:
-			return PackedByteArray(stream.get_data(stream.get_u16())).to_int32_array(),
+			return PackedByteArray(stream.get_data(_get_dynamic_int(stream))[1]).to_int32_array(),
 	},
 	TYPE_PACKED_INT64_ARRAY: {
 		"pack": func(value: PackedInt64Array, stream: StreamPeerBuffer) -> StreamPeerBuffer:
 			var buffer = value.to_byte_array()
-
-			stream.put_u16(buffer.size())
+			_put_dynamic_int(buffer.size(), stream)
 			stream.put_data(buffer)
-
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> PackedInt64Array:
-			return PackedByteArray(stream.get_data(stream.get_u16())).to_int64_array(),
+			return PackedByteArray(stream.get_data(_get_dynamic_int(stream))[1]).to_int64_array(),
 	},
 	TYPE_PACKED_FLOAT32_ARRAY: {
 		"pack": func(value: PackedFloat32Array, stream: StreamPeerBuffer) -> StreamPeerBuffer:
 			var buffer = value.to_byte_array()
-
-			stream.put_u16(buffer.size())
+			_put_dynamic_int(buffer.size(), stream)
 			stream.put_data(buffer)
-
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> PackedFloat32Array:
-			return PackedByteArray(stream.get_data(stream.get_u16())).to_float32_array(),
+			return PackedByteArray(stream.get_data(_get_dynamic_int(stream))[1]).to_float32_array(),
 	},
 	TYPE_PACKED_FLOAT64_ARRAY: {
 		"pack": func(value: PackedFloat64Array, stream: StreamPeerBuffer) -> StreamPeerBuffer:
 			var buffer = value.to_byte_array()
-
-			stream.put_u16(buffer.size())
+			_put_dynamic_int(buffer.size(), stream)
 			stream.put_data(buffer)
-
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> PackedFloat64Array:
-			return PackedByteArray(stream.get_data(stream.get_u16())).to_float64_array(),
+			return PackedByteArray(stream.get_data(_get_dynamic_int(stream))[1]).to_float64_array(),
 	},
 	TYPE_PACKED_STRING_ARRAY: {
 		"pack": func(value: PackedStringArray, stream: StreamPeerBuffer) -> StreamPeerBuffer:
-			stream.put_u16(value.size())
-
+			_put_dynamic_int(value.size(), stream)
 			for string in value:
 				pack(string, stream, TYPE_STRING)
-
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> PackedStringArray:
-			var array_size := stream.get_u16()
+			var array_size = _get_dynamic_int(stream)
 			var array := PackedStringArray()
-
 			for i in array_size:
 				array.append(unpack(stream, TYPE_STRING))
-
 			return array,
 	},
 	TYPE_PACKED_VECTOR2_ARRAY: {
 		"pack": func(value: PackedVector2Array, stream: StreamPeerBuffer) -> StreamPeerBuffer:
-			stream.put_u16(value.size())
-
+			_put_dynamic_int(value.size(), stream)
 			for vector in value:
 				pack(vector, stream, TYPE_VECTOR2)
-
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> PackedVector2Array:
-			var array_size := stream.get_u16()
+			var array_size = _get_dynamic_int(stream)
 			var array := PackedVector2Array()
-
 			for i in array_size:
 				array.append(unpack(stream, TYPE_VECTOR2))
-
 			return array,
 	},
 	TYPE_PACKED_VECTOR3_ARRAY: {
 		"pack": func(value: PackedVector3Array, stream: StreamPeerBuffer) -> StreamPeerBuffer:
-			stream.put_u16(value.size())
-
+			_put_dynamic_int(value.size(), stream)
 			for vector in value:
 				pack(vector, stream, TYPE_VECTOR3)
-
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> PackedVector3Array:
-			var array_size := stream.get_u16()
+			var array_size = _get_dynamic_int(stream)
 			var array := PackedVector3Array()
-
 			for i in array_size:
 				array.append(unpack(stream, TYPE_VECTOR3))
-
+			return array,
+	},
+	TYPE_PACKED_VECTOR4_ARRAY: {
+		"pack": func(value: PackedVector4Array, stream: StreamPeerBuffer) -> StreamPeerBuffer:
+			_put_dynamic_int(value.size(), stream)
+			for vector in value:
+				pack(vector, stream, TYPE_VECTOR4)
+			return stream,
+		"unpack": func(stream: StreamPeerBuffer) -> PackedVector4Array:
+			var array_size = _get_dynamic_int(stream)
+			var array := PackedVector4Array()
+			for i in array_size:
+				array.append(unpack(stream, TYPE_VECTOR4))
 			return array,
 	},
 	TYPE_PACKED_COLOR_ARRAY: {
 		"pack": func(value: PackedColorArray, stream: StreamPeerBuffer) -> StreamPeerBuffer:
-			stream.put_u16(value.size())
-
+			_put_dynamic_int(value.size(), stream)
 			for vector in value:
 				pack(vector, stream, TYPE_COLOR)
-
 			return stream,
 		"unpack": func(stream: StreamPeerBuffer) -> PackedColorArray:
-			var array_size := stream.get_u16()
+			var array_size = _get_dynamic_int(stream)
 			var array := PackedColorArray()
-
 			for i in array_size:
 				array.append(unpack(stream, TYPE_COLOR))
-
 			return array,
 	},
 	CustomTypes.TYPED_ARRAY: {
 		"pack": func(array: Array, stream: StreamPeerBuffer) -> StreamPeerBuffer:
-			var item_type: int = TYPE_NIL
-
-			if _can_array_be_typed(array):
-				item_type = typeof(array[0])
-
-			stream.put_u16(array.size())
+			var item_type: int = typeof(array[0]) if array.size() > 0 else TYPE_NIL
+			_put_dynamic_int(array.size(), stream)
 			stream.put_u8(item_type)
-
 			for item in array:
 				pack(item, stream, item_type)
-
 			return stream,
-		"unpack": func(stream: StreamPeerBuffer):
-			var array_size = stream.get_u16()
+		"unpack": func(stream: StreamPeerBuffer) -> Array:
+			var array_size = _get_dynamic_int(stream)
 			var item_type = stream.get_u8()
-
-			if item_type == TYPE_NIL:
-				return null
-
 			var array = []
 			for i in array_size:
 				array.append(unpack(stream, item_type))
-
 			return array,
 	}
 }
@@ -525,31 +470,30 @@ static func unpack_from_bytes(bytes: PackedByteArray):
 	stream.data_array = bytes
 	return unpack(stream)
 
-static func pack(value, stream:=StreamPeerBuffer.new(), type:=-1, add_type_prefix:=false) -> StreamPeerBuffer:
-	add_type_prefix = add_type_prefix or type == -1 # If type is unknown we add a type prefix to identify it
+static func pack(value, stream := StreamPeerBuffer.new(), type := -1, add_type_prefix := false) -> StreamPeerBuffer:
+	add_type_prefix = add_type_prefix or type == -1
 
 	if type == -1:
-		type = typeof(value) # Check the generic type of the value
+		type = typeof(value)
 
-		# If the type is an array, lets try to make it typed (to save data)
-		if type == TYPE_ARRAY and _can_array_be_typed(value):
-			type = CustomTypes.TYPED_ARRAY
-		if type == TYPE_INT:
-			type = _get_int_type(value)
-		if type == TYPE_OBJECT and value == null:
-			type = TYPE_NIL
+	if type == TYPE_ARRAY and _can_array_be_typed(value):
+		type = CustomTypes.TYPED_ARRAY
+	if type == TYPE_INT:
+		type = _get_int_type(value)
+	if type == TYPE_OBJECT and value == null:
+		type = TYPE_NIL
 
 	if not type in _serializer:
 		push_error("[Seriously] Unknown type: ", type)
-		return null
+		return stream
 
 	if add_type_prefix:
 		stream.put_u8(type)
 
 	return _serializer[type].pack.call(value, stream)
 
-static func unpack(stream: StreamPeerBuffer, type:=-1):
-	if type == -1: # If we dont define a type we try to read a type prefix
+static func unpack(stream: StreamPeerBuffer, type := -1):
+	if type == -1:
 		type = stream.get_u8()
 
 	if not type in _serializer:
@@ -558,37 +502,50 @@ static func unpack(stream: StreamPeerBuffer, type:=-1):
 
 	return _serializer[type].unpack.call(stream)
 
-# Private methods
-static func _can_array_be_typed(array: Array) -> bool:
-	if array.size() == 0: # Typing empty arrays makes no sense
-		return false
-	if array.size() == 1:
-		return true # If an array has just 1 entry thats 1 type. So it CAN be typed
+#region private methods
+static func _put_dynamic_int(value: int, stream: StreamPeerBuffer):
+	while true:
+		var b = value & 0x7F
+		value >>= 7
+		if value > 0:
+			stream.put_u8(b | 0x80)
+		else:
+			stream.put_u8(b)
+			break
 
-	var array_type: int = typeof(array[0]) # Lets check if all other entries have the same type
+static func _get_dynamic_int(stream: StreamPeerBuffer) -> int:
+	var result = 0
+	var shift = 0
+	while true:
+		var b = stream.get_u8()
+		result |= (b & 0x7F) << shift
+		if (b & 0x80) == 0:
+			break
+		shift += 7
+	return result
+
+static func _can_array_be_typed(array: Array) -> bool:
+	if array.is_typed():
+		return true
+	if array.size() <= 1: 
+		return false
+
+	var array_type: int = typeof(array[0])
 	for entry in array:
-		if typeof(entry) != array_type: return false # This array cannot be typed because there is a mismatch
+		if typeof(entry) != array_type: 
+			return false 
 
 	return true
 
 static func _get_int_type(value: int) -> int:
-	var unsigned = value >= 0
-	var bit_size = 8
-	if abs(value) <= 0xFF:
-		bit_size = 8
-	elif abs(value) <= 0xFFFF:
-		bit_size = 16
-	elif abs(value) <= 0xFFFFFFFF:
-		bit_size = 32
-	elif value >= INT_MIN and value <= INT_MAX:
-		bit_size = 64
+	if value >= 0:
+		if value <= 255: return CustomTypes.UINT8
+		if value <= 65535: return CustomTypes.UINT16
+		if value <= 4294967295: return CustomTypes.UINT32
+		return CustomTypes.UINT64
 	else:
-		push_error("[Seriously] Unsupported integer: ", value)
-		return -1
-
-	var type_name := "%sINT%s" % ["U" if unsigned else "", bit_size]
-	if not type_name in CustomTypes:
-		push_error("[Seriously] Unsupported integer: ", value)
-		return -1
-
-	return CustomTypes[type_name]
+		if value >= -128: return CustomTypes.INT8
+		if value >= -32768: return CustomTypes.INT16
+		if value >= -2147483648: return CustomTypes.INT32
+		return CustomTypes.INT64
+#endregion
